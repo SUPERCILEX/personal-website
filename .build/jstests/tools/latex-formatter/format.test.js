955cc10dbea5c1ced25417770a6ac7b15c410be7
Alex Saveau <saveau.alexandre@gmail.com>
2020-11-04 23:10:10 -0800

    Don't apply math formatting outside equation blocks
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ .build/jstests/tools/latex-formatter/format.test.js
@@ -0,0 +1,247 @@
+const formatting = require('../../../../tools/latex-formatter/index');
+
+test('empty document does nothing', () => {
+  expect(formatting.format()).toBe('');
+  expect(formatting.format('')).toBe('');
+});
+
+test('document without begin{document} does nothing', () => {
+  const document = `blah blah
+\\begin{question}
+blah`;
+
+  expect(formatting.format(document)).toBe(document);
+});
+
+test('imports are ignored', () => {
+  const document = `blah-blah
+    foobar
+
+\\begin{document}
+stuff
+\\end{document}
+`;
+
+  expect(formatting.format(document)).toBe(document);
+});
+
+test('environments are indented', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}
+    \\begin{b}
+    \\begin{c}
+    \\end{c}
+    \\begin{e}
+    \\end{e}
+    \\end{b}
+    \\end{a}
+
+    \\begin{d}
+    \\end{d}
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    \\begin{b}
+        \\begin{c}
+        \\end{c}
+        \\begin{e}
+        \\end{e}
+    \\end{b}
+\\end{a}
+
+\\begin{d}
+\\end{d}
+\\end{document}
+`);
+});
+
+// TODO fix
+test.skip('minted code is de-indented', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}
+    \\begin{minted}{python}
+    def foo():
+        1 + 1
+    \\end{minted}
+    \\end{a}
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    \\begin{minted}{python}
+def foo():
+    1 + 1
+    \\end{minted}
+\\end{a}
+\\end{document}
+`);
+});
+
+test('minted code isn\'t modified', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}
+    \\begin{minted}{python}
+1+1-1==1
+    \\end{minted}
+    \\end{a}
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    \\begin{minted}{python}
+1+1-1==1
+    \\end{minted}
+\\end{a}
+\\end{document}
+`);
+});
+
+test('compressed math is formatted correctly', () => {
+  const math = 'a=b+1-2^{5}\\cdot\\log(2)';
+
+  expect(formatting.format(`
+    \\begin{document}
+    $${math}$
+    $
+    ${math}
+    $
+
+    $$${math}$$
+    $$
+    ${math}
+    $$
+
+    \\begin{align*}
+    ${math}
+    \\end{align*}
+    \\end{document}`)).toBe(`\\begin{document}
+$a = b + 1 - 2^5 \\cdot \\log(2)$
+$
+a = b + 1 - 2^5 \\cdot \\log(2)
+$
+
+$$a = b + 1 - 2^5 \\cdot \\log(2)$$
+$$
+a = b + 1 - 2^5 \\cdot \\log(2)
+$$
+
+\\begin{align*}
+    a = b + 1 - 2^5 \\cdot \\log(2)
+\\end{align*}
+\\end{document}
+`);
+});
+
+test('uncompressed math is formatted correctly', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    $a   =  b  + 1  -   2^{5}   \\cdot         \\log(2)$
+    \\end{document}`)).toBe(`\\begin{document}
+$a = b + 1 - 2^5 \\cdot \\log(2)$
+\\end{document}
+`);
+});
+
+test('math is ignored outside of equation blocks', () => {
+  const math = 'a=b+1-2^{5}\\cdot\\log(2)';
+
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}
+    $$blah$$
+    \\\\$blah$
+    \\$${math}\\$
+    \\end{a}
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    $$blah$$
+    \\\\$blah$
+    \\$${math}\\$
+\\end{a}
+\\end{document}
+`);
+});
+
+test('trailing whitespace is stripped', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}  
+    blah   
+    \\end{a}  
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    blah
+\\end{a}
+\\end{document}
+`);
+});
+
+test('sentences are separated by a new line', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    \\begin{a}  
+    'a.b.c.' is cool. So is this. Blah.   
+    \\end{a}  
+    \\end{document}`)).toBe(`\\begin{document}
+\\begin{a}
+    'a.b.c.' is cool.
+    So is this.
+    Blah.
+\\end{a}
+\\end{document}
+`);
+});
+
+test('dots inside equation blocks are ignored', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    $\\forall f. (...)$
+    $$\\forall f. (...)$$
+    \\begin{align*}
+    \\forall f. (...)
+    \\end{align*}
+    \\end{document}`)).toBe(`\\begin{document}
+$\\forall f. (...)$
+$$\\forall f. (...)$$
+\\begin{align*}
+    \\forall f. (...)
+\\end{align*}
+\\end{document}
+`);
+});
+
+test('minus sign is not formatted when used as negation', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    $a - b, a-b, a- b, a -b$
+    $$blah$$
+    $-1$
+    \\end{document}`)).toBe(`\\begin{document}
+$a - b, a - b, a - b, a -b$
+$$blah$$
+$-1$
+\\end{document}
+`);
+});
+
+test('&= is kept together', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    $&=foo$
+    $&= foo$
+    \\end{document}`)).toBe(`\\begin{document}
+$&= foo$
+$&= foo$
+\\end{document}
+`);
+});
+
+test('=\\; is kept together', () => {
+  expect(formatting.format(`
+    \\begin{document}
+    $=\\;foo$
+    $=\\; foo$
+    \\end{document}`)).toBe(`\\begin{document}
+$=\\;foo$
+$=\\; foo$
+\\end{document}
+`);
+});
--- tools/latex-formatter/index.js
+++ tools/latex-formatter/index.js
@@ -7,46 +7,133 @@ function formatLatex() {
 }
 
 function format(latex) {
+  if (!latex) {
+    return '';
+  }
+  latex = latex.trim();
+
   const docStartIndex = latex.indexOf('\\begin{document}');
+  if (docStartIndex < 0) {
+    return latex;
+  }
+
   const ignored = latex.substring(0, docStartIndex);
-  latex = latex.substring(docStartIndex)
-      .replaceAll(/ +\n/gi, '\n')
-      .replaceAll(/\. +/gi, '.\n')
-      .replaceAll(/(\S)=/gi, '$1 =')
-      .replaceAll(/=(\S)/gi, '= $1')
-      .replaceAll(/& =/gi, '&=')
-      .replaceAll(/(\S)\+/gi, '$1 +')
-      .replaceAll(/\+(\S)/gi, '+ $1')
-      .replaceAll(/(\S)-/gi, '$1 -')
-      .replaceAll(/-(\S)/gi, '- $1')
-      .replaceAll(/(\S)\\cdot/gi, '$1 \\cdot')
-      .replaceAll(/\\cdot(\S)/gi, '\\cdot $1')
-      .replaceAll(/\^{(\w)}/gi, '^$1');
+  latex = latex.substring(docStartIndex);
 
   const lines = latex.split('\n');
   latex = '';
 
-  let level = -1;
+  let equationStart = -1;
+  let environmentStack = [];
   for (let line of lines) {
     let hasBegin = line.includes('\\begin');
     let hasEnd = line.includes('\\end');
 
     if (hasEnd && !hasBegin) {
-      level--;
+      environmentStack.pop();
     }
 
-    const formattedLine = line.trimStart();
+    const insideMintedEnvironment = environmentStack.some(
+        s => ['minted'].includes(s.environment));
+    const insideEquationEnvironment = environmentStack.some(
+        s => ['align*'].includes(s.environment));
+
+    let formattedLine;
+    if (insideMintedEnvironment) {
+      formattedLine = line.trimEnd();
+    } else {
+      formattedLine = line.trim();
+    }
 
     if (formattedLine.length > 0) {
-      latex += ' '.repeat(Math.max(0, level) * 4);
+      const indent = ' '.repeat(Math.max(0, environmentStack.length - 1) * 4);
+
+      if (insideEquationEnvironment) {
+        formattedLine = formatMath(formattedLine);
+      } else {
+        let i = 0;
+        for (let c of formattedLine) {
+          const beforeLength = formattedLine.length;
+          const prevChar = formattedLine[i - 1];
+          const prevPrevChar = formattedLine[i - 2];
+
+          if (c === '$' && (prevChar !== '\\' || prevPrevChar === '\\')) {
+            if (prevChar === '$') {
+              if (equationStart !== -1) {
+                equationStart++;
+              }
+            } else {
+              if (equationStart === -1) {
+                equationStart = i + 1;
+              } else {
+                formattedLine = replace(equationStart, i, formattedLine,
+                    formatMath(formattedLine.substring(equationStart, i)));
+                equationStart = -1;
+              }
+            }
+          }
+          if (equationStart === -1 && c === ' ' && prevChar === '.') {
+            formattedLine = replace(i, i + 1, formattedLine, '\n' + indent);
+          }
+
+          i += 1 + formattedLine.length - beforeLength;
+        }
+
+        if (equationStart !== -1) {
+          formattedLine = replace(0, formattedLine.length, formattedLine,
+              formatMath(formattedLine));
+          equationStart = 0;
+        }
+      }
+
+      if (!insideMintedEnvironment) {
+        latex += indent;
+      }
     }
+
     latex += formattedLine;
     latex += '\n';
 
     if (hasBegin && !hasEnd) {
-      level++;
+      const begin = '\\begin{';
+      let environment = line.substring(line.indexOf(begin) + begin.length);
+      environment = environment.substring(0, environment.indexOf('}'));
+
+      environmentStack.push({
+        environment,
+      });
     }
   }
 
   return ignored + latex;
+}
+
+function formatMath(math) {
+  return math
+      .replace(/\s+=\s+/gi, '=')
+      .replace(/(\S)=/gi, '$1 =')
+      .replace(/=(\S)/gi, '= $1')
+      .replace(/& =/gi, '&=')
+      .replace(/= \\;/gi, '=\\;')
+      .replace(/\s+\+\s+/gi, '+')
+      .replace(/(\S)\+/gi, '$1 +')
+      .replace(/\+(\S)/gi, '+ $1')
+      .replace(/\s+-\s+/gi, '-')
+      .replace(/(\S)-(\S)/gi, '$1 - $2')
+      .replace(/(\S)-/gi, '$1 -')
+      .replace(/\s+\\cdot\s+/gi, '\\cdot')
+      .replace(/(\S)\\cdot/gi, '$1 \\cdot')
+      .replace(/\\cdot(\S)/gi, '\\cdot $1')
+      .replace(/\^{(\w)}/gi, '^$1');
+}
+
+function replace(start, end, existing, replacement) {
+  return existing.substring(0, start) + replacement + existing.substring(end);
+}
+
+try {
+  module.exports = exports = {
+    format,
+  };
+} catch (ignored) {
 }
