92da524c47edcc9ab6476adc1feee1701493ca9c
Alex Saveau <saveau.alexandre@gmail.com>
2021-04-06 19:45:52 -0700

    Use standard Jekyll environment variable
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- _plugins/post_redirects.rb
+++ _plugins/post_redirects.rb
@@ -23,8 +23,7 @@ module PostRedirects
   end
 
   Jekyll::Hooks.register :site, :post_write do |site|
-    production = site.config['env'].nil? || site.config['env'] =~ /production/i
-    if production
+    if Jekyll::env =~ /production/i
       system 'python3 .build/firebase_redirect_inliner.py' or raise 'Redirect creation failed.'
       system 'python3 .build/downgrade_seo_image_resolution.py' or raise 'SEO image res downgrader failed.'
     end
