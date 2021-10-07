52c74084692f3a16fbfc1e8c9340cb148995886a
Alex Saveau <saveau.alexandre@gmail.com>
2021-10-06 23:01:36 -0700

    Fix bad build step ordering
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- _plugins/post_redirects.rb
+++ _plugins/post_redirects.rb
@@ -24,6 +24,10 @@ module PostRedirects
 
   Jekyll::Hooks.register :site, :post_write do |site|
     if Jekyll::env =~ /production/i
+      # Jekyll's hook ordering is garbage so we have to re-run compression here just to make sure
+      # it actually ran. Couldn't get the priority param to work.
+      system 'python3 .build/image_compressor.py' or raise 'Image compression failed.'
+
       system 'python3 .build/firebase_redirect_inliner.py' or raise 'Redirect creation failed.'
       system 'python3 .build/downgrade_seo_image_resolution.py' or raise 'SEO image res downgrader failed.'
     end
