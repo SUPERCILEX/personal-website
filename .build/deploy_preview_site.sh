0f02413cd44d26604636e184777a90f15072d696
Alex Saveau <saveau.alexandre@gmail.com>
2022-02-23 22:36:26 -0800

    Use stable preview ID
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- .build/deploy_preview_site.sh
+++ .build/deploy_preview_site.sh
@@ -6,6 +6,6 @@ echo "Starting the Jekyll Action"
 ./.build/build_site.sh
 
 echo -e "\nDeploying to Firebase Hosting"
-./.build/node_modules/.bin/firebase hosting:channel:deploy "$BUILD_ID"
+./.build/node_modules/.bin/firebase hosting:channel:deploy "$GITHUB_REF_NAME"
 
 exit $?
